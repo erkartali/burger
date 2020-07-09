dfb6a4e408225fb6fda23358063de817a176e370
erkartali <erkartali@gmail.com>
2020-07-09 14:20:19 -0500

    added connection settings for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "flipflip",
-  database: "burger_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "flipflip",
+    database: "burger_db",
+  });
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
